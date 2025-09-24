bdfa3d7133f5b38adeff15ec1cce626009679f5c
jmyers7 <jmmyers25@gmail.com>
2025-09-24 09:22:16 -0400

    commit

--- posts/shiny-clt/script.R
+++ posts/shiny-clt/script.R
@@ -10,36 +10,53 @@ grey <- custom_colors[["grey"]]
 
 theme_set(custom_theme())
 
-meanlog <- 1
-sdlog <- 0.75
-n.samples <- 10
-n.replicates <- 1000
+# sigma <- sqrt(as.numeric(weights %*% second.moments - (weights %*% means) ** 2) / n.sample)
+# components <- sample(
+#   1:n.components,
+#   size = n.replicates * n.sample,
+#   replace = TRUE,
+#   prob = weights
+# )
 
-bootstrap.samples <- matrix(
-  rlnorm(n = n.samples * n.replicates, meanlog = meanlog, sdlog = sdlog),
-  nrow = n.replicates,
-  ncol = n.samples
-)
+# boot.sample <- matrix(
+#   rnorm(n.replicates * n.sample, mean = means[components], sd = sds[components]),
+#   nrow = n.replicates,
+#   ncol = n.sample
+# )
 
-xbars <- data.frame(xbar = rowMeans(bootstrap.samples))
+# sample.dist <- rowMeans(boot.sample)
 
-ggplot(xbars, aes(x = xbar)) +
-  geom_histogram(
-    aes(y = after_stat(density)),
-    fill = yellow,
-    color = grey,
-    bins = 50,
-    alpha = 0.5
-  ) +
-  stat_function(
-    fun = function(x) dlnorm(x, meanlog = meanlog, sdlog = sdlog),
-    color = blue,
-    n = 200
-  ) +
-  xlim(0, 13)
 
-ggplot(xbars, aes(x = xbar)) +
-  stat_density(
-    geom = "line",
-    color = yellow
-  )
+plot.sampling.dist <- function(df.sample,
+  n.sample,
+  mu, sigma,
+  bins = 50,
+  alpha = 0.5) {
+  
+  ggplot(df.sample, aes(x = xbar)) +
+    geom_histogram(
+      aes(y = after_stat(density)),
+      color = grey,
+      bins = bins,
+      alpha = alpha,
+      fill = yellow
+    ) +
+    stat_density(
+      aes(color = "data"),
+      geom = "line",
+      linewidth = 1
+    ) +
+    stat_function(
+      aes(color = "normal"),
+      fun = function(x) dnorm(x, mean = mu, sd = sigma),
+      linewidth = 1   
+    ) + 
+    scale_color_manual(
+      name = NULL,
+      values = c("data" = yellow, "normal" = blue)
+    ) +
+    labs(
+      x = TeX("$\\bar{x}$"),
+      title = TeX(paste0("sampling distribution for $\\bar{X}_{", n.sample, "}$"))
+    )
+}
